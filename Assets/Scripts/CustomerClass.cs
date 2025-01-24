95f4f5a63da585c57791e1a06c31f1e696c15981
Umberto Arduini <arduiniumberto@gmail.com>
2025-01-24 15:25:32 +0100

    feat: implemented randomized character

--- Assets/Scripts/CustomerClass.cs
+++ Assets/Scripts/CustomerClass.cs
@@ -4,31 +4,74 @@ public class CustomerClass : MonoBehaviour
 {
     public MarketClass market;
 
+    [Header("Body Part Renderers")]
+    public SpriteRenderer head;
+    public SpriteRenderer body;
+    public SpriteRenderer legR;
+    public SpriteRenderer legL;
+    public SpriteRenderer armR;
+    public SpriteRenderer armL;
+
+    [Header("Body Part Sprite Collections")]
+    public Sprite[] headSprites;
+    public Sprite[] bodySprites;
+    public Sprite[] legSprites;
+    public Sprite[] armSprites;
+
     private void Start()
     {
+        RandomizeBodyParts();
         AssignRandomMarket();
+    }
+
+    private void RandomizeBodyParts()
+    {
+        if (headSprites.Length > 0)
+            head.sprite = headSprites[Random.Range(0, headSprites.Length)];
+
+        if (bodySprites.Length > 0)
+            body.sprite = bodySprites[Random.Range(0, bodySprites.Length)];
+
+        if (legSprites.Length > 0)
+        {
+            Sprite selectedLeg = legSprites[Random.Range(0, legSprites.Length)];
+            legR.sprite = selectedLeg;
+            legL.sprite = selectedLeg;
+        }
+
+        if (armSprites.Length > 0)
+        {
+            Sprite selectedArm = armSprites[Random.Range(0, armSprites.Length)];
+            armR.sprite = selectedArm;
+            armL.sprite = selectedArm;
+        }
     }
 
     private void AssignRandomMarket()
     {
         if (MarketManager.Instance != null && MarketManager.Instance.markets.Count > 0)
         {
-            // Get a random market from the available markets
             int randomIndex = Random.Range(0, MarketManager.Instance.markets.Count);
             market = MarketManager.Instance.markets[randomIndex];
             
-            // Optional: You can add visual feedback here
-            // For example, tint the customer sprite with the market color
-            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-            if (spriteRenderer != null && market.marketData != null)
+            if (market.marketData != null)
             {
-                spriteRenderer.color = market.marketData.marketColor;
+                Color marketColor = market.marketData.marketColor;
+                ApplyColorToBodyParts(marketColor);
             }
-
         }
         else
         {
             Debug.LogError("No markets available to assign to customer!");
         }
+    }
+
+    private void ApplyColorToBodyParts(Color color)
+    {
+        body.color = color;
+        legR.color = color;
+        legL.color = color;
+        armR.color = color;
+        armL.color = color;
     }
 }
